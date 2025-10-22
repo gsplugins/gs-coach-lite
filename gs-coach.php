ffec04bd47542e0556926fb9d96d97ed1877b892
Mustafiz Munna <munna.fswd@gmail.com>
2025-10-22 14:10:42 +0600

    One step further

--- gs-coach.php
+++ gs-coach.php
@@ -31,32 +31,17 @@ if (!defined('ABSPATH')) exit;
  * Defining constants
  */
 if (!defined('GSCOACH_VERSION')) define('GSCOACH_VERSION', '2.5.8');
+if (!defined('GSCOACH_MIN_PRO_VERSION')) define('GSCOACH_MIN_PRO_VERSION', '2.0.0');
 if (!defined('GSCOACH_MENU_POSITION')) define('GSCOACH_MENU_POSITION', 39);
+if (!defined('GSCOACH_PLUGIN_FILE')) define('GSCOACH_PLUGIN_FILE', __FILE__);
 if (!defined('GSCOACH_PLUGIN_DIR')) define('GSCOACH_PLUGIN_DIR', plugin_dir_path(__FILE__));
 if (!defined('GSCOACH_PLUGIN_URI')) define('GSCOACH_PLUGIN_URI', plugins_url('', __FILE__));
-if (!defined('GSCOACH_PLUGIN_FILE')) define('GSCOACH_PLUGIN_FILE', __FILE__);
+if (!defined('GSCOACH_PRO_PLUGIN')) define('GSCOACH_PRO_PLUGIN', 'gs-coach-pro/gs-coach-pro.php');
 
 if( ! function_exists( 'is_pro_valid' ) ){
     function is_pro_valid(){
         return true;
     }
-}
-
-
-if ( ! is_pro_valid() ) {
-    function gs_coach_free_vs_pro_page() {
-
-        add_submenu_page(
-            'edit.php?post_type=gs_coaches',
-            'Free Pro Trial',
-            'Free Pro Trial',
-            'delete_posts',
-            'free-pro-trial',
-        );
-    
-    }
-    add_action( 'admin_menu', 'gs_coach_free_vs_pro_page', 20 );
-
 }
 
 require_once GSCOACH_PLUGIN_DIR . 'includes/autoloader.php';
